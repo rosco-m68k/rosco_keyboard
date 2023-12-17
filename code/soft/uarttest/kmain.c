29e0a7249898ff62ff0fc545b48c4a0cd1905a66
Ross Bamford <roscopeco@gmail.com>
2023-12-17 00:15:41 +0000

    Update sample code

--- code/soft/uarttest/kmain.c
+++ code/soft/uarttest/kmain.c
@@ -6,15 +6,154 @@
 #include <stdint.h>
 #include <buffered_uart.h>
 
-static RingBuffer uart_b;
+#define CMD_IDENT           ((uint8_t)0xf0)
+#define CMD_ACK             ((uint8_t)0xff)
+#define IDENT_MODE_ASCII    ((uint8_t)0x01)
+
+// static RingBuffer uart_b;
 static CharDevice device;
 uint8_t buf;
+volatile long * ticks = (volatile long*)0x40c;
+uint8_t step;
+
+int tryGetChar(CharDevice *device) {
+    long end = *ticks + 20;
+
+    while (*ticks < end) {
+        if (mcCheckDevice(device)) {
+            return (uint8_t)mcReadDevice(device);
+        }
+    }
+
+    return -1;
+}
+
+bool detectKeyboard(CharDevice *device) {
+    step = 0;
+
+    // Clear buffer
+    while (mcCheckDevice(device)) {
+        mcReadDevice(device);
+    }
+
+    // Send "IDENT" command
+    mcSendDevice(CMD_IDENT, device);
+
+    int chr = tryGetChar(device);
+    if (chr != 0x72) {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'o') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 's') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'c') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'o') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != '_') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'k') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'b') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 'd') {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != IDENT_MODE_ASCII) {
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr == -1) {    // key count
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr == -1) {    // led count
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr == -1) {    // capabilities
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 0) {     // reserved - must be 0
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != 0) {     // reserved2 - must be 0
+        return false;
+    }
+
+    step++;
+
+    chr = tryGetChar(device);
+    if (chr != CMD_ACK) {   // required ack
+        return false;
+    }
+
+    return true;
+}
 
 void kmain() {
-    int i = 0;
-    int n = 1;
-
-    printf("Install handlers\n");
+    printf("Detect Keyboard\n");
 
     if (!mcCheckDeviceSupport()) {
         printf("ERROR: No device support\n");
@@ -31,32 +170,43 @@ void kmain() {
         return;
     }
 
+    if (!detectKeyboard(&device)) {
+        printf("ERROR: No keyboard found [step: %d]\n", step);
+        return;
+    }
+
+#ifdef INTERRUPT_DRIVEN
+    int n;
     duart_install_interrupt(&device, NULL, &uart_b);
-
-    printf("Righto, that's done...\n");
-
-    int led_on = 0;
+#endif
+    printf("Righto, type away...\n");
 
     while (true) {
-        if (i++ == 3) {
-            mcSendDevice(0x5, &device);
-
-            if (led_on) {
-                mcSendDevice(0, &device);
-                led_on = 0;
-            } else {
-                mcSendDevice((unsigned char)255, &device);
-                led_on = 1;
-            }
-            i = 0;
-        }
-
-        buf = mcReadDevice(&device);
-        // n = duart_unbuffer_one(&uart_b, &buf);
-
+#ifdef INTERRUPT_DRIVEN
+        n = duart_unbuffer_one(&uart_b, &buf);
         if (n) {
-            printf("%c", buf);
+#else
+            buf = mcReadDevice(&device);
+#endif
+            switch (buf) {
+            case 0x08:
+                // backspace
+                mcPrintchar(0x08);
+                mcPrintchar(0x20);
+                mcPrintchar(0x08);
+                break;
+            case 0x0a:
+            case 0x0d:
+                mcPrintchar(0x0a);
+                mcPrintchar(0x0d);
+                break;
+            default:
+                mcPrintchar(buf);
+            }
+
+#ifdef INTERRUPT_DRIVEN    
         }
+#endif
     }
 }
 
