f7adfd7bf1e4d0221a5a7525730bf187e13e741a
Ross Bamford <roscopeco@gmail.com>
2024-02-17 03:16:42 +0000

    Remove build dir

--- code/usb/.build/obj_rosco_m68k_default/common/util.d
+++
@@ -1,9 +0,0 @@
-.build/obj_rosco_m68k_default/common/util.o: tmk_core/common/util.c \
- keyboards/rosco_m68k/config.h quantum/config_common.h \
- tmk_core/common/util.h
-
-keyboards/rosco_m68k/config.h:
-
-quantum/config_common.h:
-
-tmk_core/common/util.h:
